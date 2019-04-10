31bd0ebdcec6ebbfe3e15f9638e89b4763989184
tyler-wel <tyler.welsh@seattleconsulting.co.jp>
2019-04-10 12:18:52 +0900

    added new gem faker. user authentication added. user updating/editing added. user display being added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user,  only: [:edit, :update, :index]
+  before_action :correct_user,    only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -18,10 +22,41 @@ class UsersController < ApplicationController
     end
   end 
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
   private 
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    # BEFORE FILTER FUNCTINOS
+
+    # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
     
+    # Confirms the correct logged in user
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
